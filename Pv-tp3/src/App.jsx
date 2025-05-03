13ba573891026a760e4f86e6ad2b900a91bdde9a
RMaxiG <maxiguzman06@gmail.com>
2025-05-02 21:16:12 -0300

    feat: Integracion de funcionalidades en App.jsx [RMaxiG]

--- Pv-tp3/src/App.jsx
+++ Pv-tp3/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import TaskList from './components/TaskList';
+import TaskInput from './components/TaskInput';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [tareas, setTareas] = useState([]);
+  
+  const AñadirTarea = (tarea) => {
+    setTareas((prev) => [...prev, tarea]);
+  };
+  
+  const EstadoTarea = (id) => {
+    setTareas((prev) =>
+      prev.map((tarea) =>
+        tarea.id === id ? { ...tarea, estado: !tarea.estado } : tarea
+      )
+    );
+  };
+  
+  const EliminarTarea = (id) => {
+    setTareas((prev) => prev.filter((tarea) => tarea.id !== id));
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Lista de Tareas</h1>
+      <TaskInput agregarNtarea={AñadirTarea} />
+      <TaskList tareas={tareas} EstadTarea={EstadoTarea} ElimiTarea={EliminarTarea} />
+    </div>
+  );  
 }
 
-export default App
+export default App;
+
